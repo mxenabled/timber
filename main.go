d8ee725e60ca5b7662dc9fd07347d221ed7f11da
Ben Call <bencall@19094-BCall.local>
2021-01-19 14:30:14 -0700

    does parsing and fills in the struct

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/kr/pretty"
@@ -135,11 +136,98 @@ func (self *PostgresLogParser) Parse() (*PostgresLogLine, error) {
 }
 
 func (self *PostgresLogParser) parseLogBuffer() (*PostgresLogLine, error) {
+	// Parse Duration
+	index := strings.Index(self.buffer, "duration: ")
+	if index < 0 {
+		return nil, nil 
+	}
+	durationEtc := self.buffer[index:]
+	durationEndIndex := strings.Index(durationEtc, " ms")
+	if durationEndIndex < 0 {
+		return nil, nil
+	}
+	durationEndIndex += index
+	duration, _ := time.ParseDuration(
+		fmt.Sprint(
+			strings.Replace(self.buffer[index:durationEndIndex], "duration: ", "", 1),
+			"ms"))
+
+	timestamp := parseTime(self.buffer)
+	logType := parseLogType(self.buffer)
+	user, database := parseUserAndDatabase(self.buffer)
+	statmentName, _ := parseStatementName(self.buffer)
+	// duration := parseDuration(self.buffer)
+
 	log := &PostgresLogLine{
+		Timestamp: timestamp,
+		Username: user,
+		Database: database,
+		Duration: duration,
+		LogType:  logType,
+		StatementName: statmentName,
 		Value: self.buffer,
 	}
+
 	self.buffer = ""
 	return log, nil
+}
+
+// Parse Time
+func parseTime(buffer string) (time.Time) {
+	timeStr := strings.Split(buffer, " [")[0]
+	timestamp, _ := time.Parse("2006-01-02 15:04:05 MST", timeStr)
+	return timestamp
+}
+
+// Parse Log Type
+func parseLogType(buffer string) (string) {
+	partial := strings.Split(buffer, " ms  ")[1]
+	return strings.Split(partial, ":")[0]
+}
+
+// Parse the statementName
+func parseStatementName(buffer string) (string, error) {
+	index := strings.Index(buffer, "statement: ")
+	if index < 0 {
+		return "", nil
+	}
+	return strings.Replace(buffer[index:], "statement: ", "", 1), nil
+}
+
+// // Parse the duration
+// func parseDuration(buffer string) (*time.Duration, error) {
+// 	index := strings.Index(buffer, "duration: ")
+// 	if index < 0 {
+// 		return nil, nil
+// 	}
+// 	durationEtc := buffer[index:]
+// 	durationEndIndex := strings.Index(durationEtc, " ms")
+// 	if durationEndIndex < 0 {
+// 		return nil, nil
+// 	}
+// 	durationEndIndex += index
+// 	duration, err := time.ParseDuration(
+// 		fmt.Sprint(
+// 			strings.Replace(buffer[index:durationEndIndex], "duration: ", "", 1),
+// 			"ms"))
+// 	if err != nil {
+// 		return nil, nil
+// 	}
+// 	return duration
+// }
+
+// Parse User and Database
+func parseUserAndDatabase(buffer string) (string, string) {
+	index := strings.Index(buffer, " LOG:")
+	if index < 0 {
+		return "", ""
+	}
+	splitsUpToUserAndDatabase := strings.Split(buffer[:index], " ")
+	userAndDatabaseStr := splitsUpToUserAndDatabase[len(splitsUpToUserAndDatabase)-1]
+	userAndDatabase := strings.Split(userAndDatabaseStr, "@")
+	user := userAndDatabase[0]
+	database := userAndDatabase[1]
+	return user, database
 }
 
 func isNewLogLine(line string) bool {
